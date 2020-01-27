e07a0b9ddeedf55f28ca853dd47203cecfaef4c3
Martino Fornasa <mf@fornasa.it>
2020-01-27 21:33:28 +0000

    New version

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from config import Config
 from flask_pymongo import PyMongo
+import sys, logging, json_logging
 
 app = Flask(__name__)
 app.config.from_object(Config)
 mongo = PyMongo(app)
+
+json_logging.ENABLE_JSON_LOGGING = True
+json_logging.init_flask()
+json_logging.init_request_instrument(app)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
-from app import app, mongo
-from flask import request
+from app import app, mongo, logger
+from flask import request, render_template
+
+# Importing the required objects from the Prometheus lib
+from prometheus_client import Counter, Histogram, Gauge, Summary
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+
+# Adding a custom metric: let's count all Mongo insertions
+object_counter = Counter('added_objects', 'Call to Mongo to add objects')
 
 @app.route('/')
 @app.route('/index')
 def index():
+  logger.info("Hello from Flask!")
   return "Hello, World!"
 
 @app.route('/thing/<name>', methods=["GET"])
@@ -26,4 +34,11 @@ def create_thing(name):
         "_id": name,
         "description": data['description'] or "no description"
       })
+    object_counter.inc()
+
     return 'created new thing named ' + name
+
+@app.route("/metrics", methods=["GET"])
+def metrics():
+  headers = {'Content-Type': CONTENT_TYPE_LATEST}
+  return generate_latest(), 200, headers
